8f6bcf1818de12a81e81f210f357e8e330ef5417
Valepa2911 <megp17102017@mail.ru>
2022-12-02 20:17:50 +0400

    сохран1.

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Ведите два числа: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA>numberB)
+{
+    Console.Write("Max-");
+    Console.Write(numberA);
+    Console.Write("Min-");
+    Console.Write(numberB);
+
+}
+else
+{
+    Console.Write("Max-");
+    Console.Write(numberB);
+    Console.Write("Min-");
+    Console.Write(numberA);
+}
