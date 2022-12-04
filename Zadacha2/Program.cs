99e17dd61a1b00db67d546df42ebe85c125b24d4
Valepa2911 <megp17102017@mail.ru>
2022-12-04 09:36:09 +0400

    сохран7.

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите два числа: ");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите два числа: ");
             int numA = Convert.ToInt32(Console.ReadLine());
             int numB = Convert.ToInt32(Console.ReadLine());
 
